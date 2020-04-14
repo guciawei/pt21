27e41417f031904c223c6900d1cb25c70588d824
pengting <2107648242@qq.com>
2020-04-14 15:05:48 +0800

    我也不知道啊

---
+++ app/src/main/java/com/example/pt21/service/NewsService.java
@@ -0,0 +1,5 @@
+package com.example.pt21.service;
+
+public interface NewsService {
+    void a();
+}
---
+++ app/src/main/java/com/example/pt21/service/serviceImp/NewServiceImp.java
@@ -0,0 +1,13 @@
+package com.example.pt21.service.serviceImp;
+
+import android.util.Log;
+
+import com.example.pt21.MainActivity;
+import com.example.pt21.service.NewsService;
+
+public class NewServiceImp implements NewsService {
+    @Override
+    public void a() {
+        Log.i(MainActivity.this,"asdfgfdfg");
+    }
+}
